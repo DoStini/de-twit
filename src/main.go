1c1c18350df6770dee87d208c16a103b7546273a
Nuno Alves <nyansword@gmail.com>
2022-12-06 13:44:37 +0000

    fix: import issue, add: some testing

--- src/main.go
+++ src/main.go
@@ -2,13 +2,39 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"github.com/libp2p/go-libp2p"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/multiformats/go-multiaddr"
 	"log"
 )
+
+// MakePeer takes a fully-encapsulated address and converts it to a
+// peer ID / Multiaddress pair
+func MakePeer(dest string) (peer.ID, multiaddr.Multiaddr) {
+	ipfsAddr, err := multiaddr.NewMultiaddr(dest)
+	if err != nil {
+		log.Fatalf("Err on creating host: %v", err)
+	}
+	log.Printf("Parsed: ipfsAddr = %s", ipfsAddr)
+
+	peerIDStr, err := ipfsAddr.ValueForProtocol(multiaddr.P_IPFS)
+	if err != nil {
+		log.Fatalf("Err on creating peerIDStr: %v", err)
+	}
+	log.Printf("Parsed: PeerIDStr = %s", peerIDStr)
+
+	targetPeerAddr, err := multiaddr.NewMultiaddr(fmt.Sprintf("/ipfs/%s", peerIDStr))
+	log.Printf("Created targetPeerAddr = %v", targetPeerAddr)
+
+	targetAddr := ipfsAddr.Decapsulate(targetPeerAddr)
+	log.Printf("Decapsulated = %v", targetAddr)
+
+	return peer.ID(peerIDStr), targetAddr
+}
 
 func addressWithPort(port int64) (multiaddr.Multiaddr, error) {
 	return multiaddr.NewMultiaddr(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", port))
@@ -16,8 +42,6 @@ func addressWithPort(port int64) (multiaddr.Multiaddr, error) {
 
 func createNode(port int64) host.Host {
 	privKey := generatePrivateKey(port)
-
-	fmt.Println(privKey)
 
 	addr, err := addressWithPort(port)
 	if err != nil {
@@ -38,26 +62,29 @@ func createNode(port int64) host.Host {
 }
 
 func main() {
+	port := flag.Int64("port", 4000, "The port of this host")
+	//node := flag.String()
+	flag.Parse()
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	h := createNode(4000)
+	h := createNode(*port)
+	hostID := h.ID()
 
-	addr, _ := multiaddr.NewMultiaddr("localhost:4001")
-	other, _ := libp2p.New(libp2p.Identity(generatePrivateKey(4001)), libp2p.ListenAddrs(addr))
-	fmt.Println(h.ID(), other.ID())
-
-	_, err := dht.New(ctx, h, dht.BootstrapPeers())
+	kad, err := dht.New(ctx, h, dht.BootstrapPeers(peer.AddrInfo{ID: hostID}))
 	if err != nil {
 		log.Fatalf("Err on creating dht")
 	}
-
-	// h.Peerstore().AddAddr(other.ID(), addr, 1000)
+	err = kad.Bootstrap(ctx)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
 
 	// kad.FindPeer(ctx, h.ID())
 
-	// print the node's listening addresses
-	fmt.Println("Listen addresses:", h.Addrs())
+	log.Printf("Host MultiAddress: %s/ipfs/%s", h.Addrs()[0].String(), hostID)
+	MakePeer(fmt.Sprintf("%s/ipfs/%s", h.Addrs()[0].String(), hostID))
 
 	// shut the node down
 	if err := h.Close(); err != nil {
