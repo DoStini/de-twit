3dbd05a30aa51e66aa5a2827b26b2c3cb6b910ed
Nuno Alves <nyansword@gmail.com>
2022-12-07 19:02:49 +0000

    cleanup structure

--- compile_bootstrap.sh
+++ compile_bootstrap.sh
@@ -1,2 +1,7 @@
 #!bash
+cd src/
+
 go build -o bootstrap  ./bootstrap.go ./utils.go ./dht.go ./logger.go
+
+mv bootstrap ../bootstrap
+cd ../
--- compile_node.sh
+++ compile_node.sh
@@ -1,2 +1,7 @@
 #!bash
+cd src/
+
 go build -o node  ./main.go ./utils.go ./dht.go ./logger.go
+
+mv node ../node
+cd ../
